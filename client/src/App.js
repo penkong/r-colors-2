9b7a01cdb61930169eb0fa3c5ef6d759f0a7b76e
mazdak <nazemi.works@gmail.com>
2019-10-06 08:00:44 -0700

    starter a

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        heelll db;
+      </div>
+    );
+  }
+}
+
+export default App;
